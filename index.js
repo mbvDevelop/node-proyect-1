31c8b94c6aed303b6aa0a1813c4f179083054e4d
mbvDevelop <mbv.design.co@gmail.com>
2022-04-21 00:08:14 +0200

    proyect II Node

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 // import routes
 const port = 3000
 
-const movieRouter = require('./routes/movieRouter')
+const movieRouter = require('./routes/movie.routes')
+const cinemaRouter = require('./routes/cinema.routes')
 
 // Connect to db
 mongoose.connect('mongodb://localhost:27017/movies', 
@@ -13,4 +14,5 @@ mongoose.connect('mongodb://localhost:27017/movies',
 
 app.use(express.json());
 app.use('/api/v1/movies', movieRouter)
+app.use('/api/v1/cinema', cinemaRouter)
 app.listen(port, () => console.log('Server listening port ' + port));
